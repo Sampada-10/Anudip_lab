a452388ec93e173b91575ea2cb3efea77005f37f
Sampada-10 <136876867+Sampada-10@users.noreply.github.com>
2024-05-14 09:02:59 +0530

    Add files via upload

---
+++ Practice/StoreManagement.java
@@ -0,0 +1,11 @@
+package com.Practice;
+ 
+import java.sql.*;
+public class StoreManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//class.forname("com.mysql.cj.jdbc.Driver");
+	}
+
+}
